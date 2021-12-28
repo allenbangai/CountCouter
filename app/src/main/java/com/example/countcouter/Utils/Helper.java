effded8fae5f07528a85c3845380d25cd6a340c6
allenbangai <allenkamadje@gmail.com>
2021-12-28 03:10:53 +0100

    including toastmessage and logmessage methods in helper class

--- app/src/main/java/com/example/countcouter/Utils/Helper.java
+++ app/src/main/java/com/example/countcouter/Utils/Helper.java
@@ -2,6 +2,8 @@ package com.example.countcouter.Utils;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
 
 public class Helper {
     Context context;
@@ -17,5 +19,17 @@ public class Helper {
     public Helper(Context context, Intent intent){
         this.context = context;
         this.intent = intent;
+    }
+
+    public void toastMessage(Context context, String message){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void toastMessage(String message){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void logMessage(Class TAGClass, String errorMessage){
+        Log.d("com.example.echat." + TAGClass.getName(), errorMessage);
     }
 }
