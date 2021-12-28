f1c4aeeda3d7e32e5b8a7c00715b58a6eb5ccf6a
allenbangai <allenkamadje@gmail.com>
2021-12-28 03:12:04 +0100

    including method to move from one activity to another

--- app/src/main/java/com/example/countcouter/Utils/Helper.java
+++ app/src/main/java/com/example/countcouter/Utils/Helper.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.example.countcouter.MainActivity;
+
 public class Helper {
     Context context;
     Intent intent;
@@ -32,4 +34,13 @@ public class Helper {
     public void logMessage(Class TAGClass, String errorMessage){
         Log.d("com.example.echat." + TAGClass.getName(), errorMessage);
     }
+
+    public void gotoSendActivity(String message){
+        intent = new Intent(context, MainActivity.class);
+        intent.putExtra("send", message);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+    }
+
 }
