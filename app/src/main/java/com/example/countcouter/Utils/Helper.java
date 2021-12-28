bdf86568807b27ef0e508bb44715788040686149
allenbangai <allenkamadje@gmail.com>
2021-12-28 03:09:04 +0100

    including constructor in helper class

--- app/src/main/java/com/example/countcouter/Utils/Helper.java
+++ app/src/main/java/com/example/countcouter/Utils/Helper.java
@@ -1,4 +1,21 @@
 package com.example.countcouter.Utils;
 
+import android.content.Context;
+import android.content.Intent;
+
 public class Helper {
+    Context context;
+    Intent intent;
+
+    public Helper(){
+    }
+
+    public Helper(Context context){
+        this.context = context;
+    }
+
+    public Helper(Context context, Intent intent){
+        this.context = context;
+        this.intent = intent;
+    }
 }
